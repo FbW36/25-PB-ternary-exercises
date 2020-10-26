d29ea97aab56039edbe382ee3f18e044a7e0fc14
INes <inesfcnogueira@gmail.com>
2020-10-26 16:05:16 +0100

    Ines

---
+++ Ines/index.js
@@ -0,0 +1,29 @@
+//1
+let isDog = false;
+console.log(isDog?"pat, pat" : "find me a dog to pat!");
+
+//2
+let speedCheck = 48;
+console.log(speedCheck>=50?"you're going too fast":"You're driving below the speed limit, Oma");
+
+//3
+let age = 14;
+console.log(age<16?"serve butter beer":"serve beer");
+
+//4
+let isStudent = false;
+console.log(isStudent?"Ticket costs €5,00":"Ticket costs €12,00");
+
+//5
+let okMarie = "bread";
+console.log(okMarie.includes("cake")?"Let them eat cake":"Oh, bother");
+
+//6
+let number1 = 30;
+let number2 = 939;
+let number3 = 40.9;
+
+console.log((number1%2)===0?number1+ " is even": number1+ " is odd");
+console.log((number2%2)===0?number2+ " is even": number2+ " is odd");
+console.log((number3%2)===0?number3+ " is even": number3+ " is odd");
+
