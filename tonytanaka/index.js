88cb7b7b0c9c1b7e88d1792ce8e747a9e25dc5b1
Tony Tanaka <tony.h.tanaka@gmail.com>
2020-10-26 16:04:36 +0100

    initial commit

---
+++ tonytanaka/index.js
@@ -0,0 +1,43 @@
+// 1
+let isDog = false
+  ? console.log("pat, pat")
+  : console.log("finde me a dog to pat!");
+
+// 2
+let speed = 200;
+let speedCheck =
+  speed > 50
+    ? console.log("you're going too fast!")
+    : console.log("You're driving below the speed limit, Oma");
+
+// 3
+let age = 17;
+let ageCheck =
+  age < 16 ? console.log("serve butter beer") : console.log("serve beer");
+
+// 4
+let student = true;
+let isStudent =
+  student == true
+    ? console.log("Ticket costs €5,00")
+    : console.log("Ticket costs €12,00");
+
+// 5
+let okMarie = false;
+let isCake =
+  okMarie == true
+    ? console.log("let them eat cake")
+    : console.log("Oh, bother");
+
+// 6
+let n1 = 30;
+let checkn1 =
+  n1 % 2 == 0 ? console.log(`${n1} is even`) : console.log("it is odd");
+
+let n2 = 939;
+let checkn2 =
+  n2 % 2 == 0 ? console.log(`${n2} is even`) : console.log("it is odd");
+
+let n3 = 40.9;
+let checkn3 =
+  n3 % 2 == 0 ? console.log(`${n3} is even`) : console.log("it is odd");
